dd7695589f132a2aa9bf81ca9d600fd18229196a
Okan <okanacioglu@gmail.com>
2023-03-07 15:13:14 +0300

    icon-button-to-header

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,10 +1,11 @@
 import { useLayoutEffect } from 'react'
-import { Text, View, Image, StyleSheet, ScrollView, Button } from 'react-native'
+import { Text, View, Image, StyleSheet, ScrollView } from 'react-native'
 import { MEALS } from '../data/dummy-data'
 
 import MealDetails from '../components/MealDetails'
 import Subtitle from '../components/MealDetail/Subtitle'
 import List from '../components/MealDetail/List'
+import IconButton from '../components/IconButton'
 
 const MealDetailScreen = ({ route, navigation }) => {
   const mealId = route.params.mealId
@@ -18,7 +19,13 @@ const MealDetailScreen = ({ route, navigation }) => {
   useLayoutEffect(() => {
     navigation.setOptions({
       headerRight: () => {
-        return <Button title='Tap me!' onPress={headerButtonPressHandler} />
+        return (
+          <IconButton
+            onPress={headerButtonPressHandler}
+            icon='star'
+            color='white'
+          />
+        )
       },
     })
   }, [navigation, headerButtonPressHandler])
