a74730d7d94fdcac679d4c7e018142d03096c522
Okan <okanacioglu@gmail.com>
2023-03-08 20:31:27 +0300

    setup-redux-toolkit

--- App.js
+++ App.js
@@ -4,13 +4,15 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { createDrawerNavigator } from '@react-navigation/drawer'
 import { Ionicons } from '@expo/vector-icons'
+import { Provider } from 'react-redux'
+import { store } from './store/redux'
 
 import CategoriesScreen from './screens/CategoriesScreen'
 import MealsOverviewScreen from './screens/MealsOverviewScreen'
 import MealDetailScreen from './screens/MealDetailScreen'
 import FavoritesScreen from './screens/FavoritesScreen'
 
-import FavoritesContextProvider from './store/context/favorites-context'
+//import FavoritesContextProvider from './store/context/favorites-context'
 
 const Stack = createNativeStackNavigator()
 const Drawer = createDrawerNavigator()
@@ -55,7 +57,8 @@ export default function App() {
   return (
     <>
       <StatusBar style='light' />
-      <FavoritesContextProvider>
+      {/* <FavoritesContextProvider> */}
+      <Provider store={store}>
         <NavigationContainer>
           <Stack.Navigator
             screenOptions={{
@@ -85,7 +88,8 @@ export default function App() {
             />
           </Stack.Navigator>
         </NavigationContainer>
-      </FavoritesContextProvider>
+      </Provider>
+      {/* </FavoritesContextProvider> */}
     </>
   )
 }
